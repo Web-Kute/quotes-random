cb1bfa213c460180f6034355f9410ae00037c4d2
Ikigai <alvardsandz@gmail.com>
2024-09-25 16:07:47 +0200

    log

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ const dropdown = document.querySelector('.dropdown-menu');
 
 async function quotesApi() {
   const quotes = await fetchApiQuotes('age');
-  console.log(quotes);
+  console.log("quotes", quotes);
   // quotesDom.innerText = quotes[0].quote;
   // authorDom.innerHTML = `&mdash; ${quotes[0].author}`;
   // catDom.innerText = `${quotes[0].category}`;
