8787fd532e99908e91c676a215064700f6ecb696
Ikigai <alvardsandz@gmail.com>
2024-09-25 23:17:03 +0200

    log try 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ const dropdown = document.querySelector('.dropdown-menu');
 
 async function quotesApi(e) {
   const quotes = await fetchApiQuotes(
-    (e.target.dataset.category = categoryList[catIndex]),
+    e.target.dataset.category || categoryList[catIndex],
   );
   console.log('quotes', quotes);
 
